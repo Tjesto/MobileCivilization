e5c59555b1106718e44ee7f03ca5c1f06cecbebd
Tjesto <szycha6@gmail.com>
2015-05-31 13:44:50 +0200

    Menu added for android - check on a4.x

--- mobilecivilisation-android/src/com/example/mobilecivilisation/ui/activities/MainScreenActivityL.java
+++ mobilecivilisation-android/src/com/example/mobilecivilisation/ui/activities/MainScreenActivityL.java
@@ -4,8 +4,12 @@ import com.example.mobilecivilisation.R;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -21,4 +25,29 @@ public class MainScreenActivityL extends Activity {
 		setActionBar(toolbar);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.menus, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		int itemId = item.getItemId();
+		switch (itemId) {
+		case R.id.new_game:
+			startActivity(new Intent(this, MainGameActivity.class));
+			break;
+		case R.id.help:
+			startActivity(new Intent(this, SettingsMenuActivityL.class));
+			break;
+		default:
+			return super.onMenuItemSelected(featureId, item);
+		}
+		
+		return true;
+		
+	}
+	
 }
---
+++ mobilecivilisation-android/src/com/example/mobilecivilisation/ui/activities/SettingsMenuActivityL.java
@@ -0,0 +1,13 @@
+package com.example.mobilecivilisation.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsMenuActivityL extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
